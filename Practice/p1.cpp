f297c52de6cb70d997a0b85b08d43de729575ec1
Gopal sharma <sharmagopal7316@gmail.com>
2025-09-16 14:06:06 +0530

    first commit

---
+++ Practice/p1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0.4;
+    cout<<a <<endl;
+
+    float b=0.4;
+    cout<<b <<endl;
+
+    double c=0.4;
+    cout<<c <<endl;
+
+    return 0;
+}
